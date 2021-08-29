9800cd39e54b5d418280f40bb019e542a211f135
MuthammaNM <87317973+MuthammaNM@users.noreply.github.com>
2021-08-29 15:19:46 +0530

    Add files via upload

---
+++ BulkfileRenamer.py
@@ -0,0 +1,17 @@
+import os
+
+def renamer():
+    i=0
+    path = "C:\\Users\\Namratha Muthamma\\OneDrive\\Documents\\data analyst\\Vs code\\img\\"
+    for filename in os.listdir(path):
+        names = f"image .{i}.jpg"
+        src = path + filename
+        dist = path +names
+
+        os.rename(src,dist)
+        i=i+1
+
+if __name__ == "__main__":
+    renamer()
+
+        
